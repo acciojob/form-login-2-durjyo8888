79809ac20891c3624e4393ecfe110bb1b2a10f44
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-11-11 21:04:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,12 +5,9 @@ function displayFormData() {
 	const phoneNumber = document.getElementById('phoneNumber').value;
 	const email = document.getElementById('email').value;
 
-	const formData = '
-	
-	First Name:${firstName}
-	Last Name:${lastName}
-	Phone Number:${phoneNumber}
-	Email Id:${email}
-	';
-	alert(formData)
+	var alertMessage = "First Name: " + firstName + "\n"
+                             + "Last Name: " + lastName + "\n"
+                             + "Phone Number: " + phoneNumber + "\n"
+                             + "Email ID: " + email;
+	alert(alertMessage)
 }
